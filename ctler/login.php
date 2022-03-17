1d1f42518c664978ba96f02c8c09265661b9a04b
ArChlOgY <52102123+ArChlOgY@users.noreply.github.com>
2022-03-17 09:39:45 +0100

    fix login check
    Reject user if login info are incorrect

--- ctler/login.php
+++ ctler/login.php
@@ -6,6 +6,8 @@ $pass = $_POST['password'];
 if (!empty($email) && !empty($pass)) {
     if ($email == "azerty@foreach.com" &&  $pass == "1234") {
         header("Location:../index.php?return=success");
+    } else {
+        header("Location:../index.php?return=error");
     }
 }else{
         header("Location:../index.php?return=error");
